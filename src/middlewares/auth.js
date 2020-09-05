7c31468379c9e1e5fa62fbcb9d77386fb31bda6c
YeonGyu Kim <code.yeon.gyu@gmail.com>
2020-09-05 18:40:35 +0900

    Add a middleware for jwt authentication

---
+++ src/middlewares/auth.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken")
+const secretObj = require("../config/jwt.js")
+
+module.exports = (req, res, next) => {
+    const authHeader = req.headers['authorization']
+    if (authHeader === undefined) {
+        res.sendStatus(401)
+        return
+    }
+    const authMethod = authHeader.slice(0, 3)
+    const jwtToken = authHeader.slice(4)
+    if (authMethod !== 'jwt') {
+        res.sendStatus(401)
+        return
+    }
+    jwt.verify(jwtToken, secretObj.secret, (err, decoded) => {
+        if (!err) {
+            req.username = decoded.username
+            next()
+        } else {
+            console.log(err)
+            res.send(400, err.name)
+            return
+        }
+    })
+}
