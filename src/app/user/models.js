ad1e6ca1bde7d66fa802055caa9bd59548646a71
YeonGyu Kim <code.yeon.gyu@gmail.com>
2020-09-05 18:36:27 +0900

    Add models and controllers for user CRUD

---
+++ src/app/user/models.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const bcrypt = require('bcrypt')
+
+
+const userSchema = new Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    nickname: { type: String }
+})
+
+userSchema.index({ username: 1 })
+
+userSchema.methods.createPassword = async (password) => {
+    const bcrypt = require('bcrypt');
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds)
+    const hash = await bcrypt.hash(password, salt)
+    return hash
+}
+
+userSchema.methods.checkPassword = async (password) => {
+    return await bcrypt.compare(password, this.password)
+}
+
+module.exports = mongoose.model('User', userSchema)
