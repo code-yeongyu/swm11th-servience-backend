9354a14378a2aaa091c4a79504fa5e6cea96d2db
YeonGyu Kim <code.yeon.gyu@gmail.com>
2020-09-09 17:11:29 +0900

    Update routes.js to improve readability
    Seperate the validator codes

--- src/app/user/routes.js
+++ src/app/user/routes.js
@@ -8,16 +8,24 @@ const { body } = require('express-validator');
 const errorCode = require('../../errors/codes.js')
 const errorWithMessage = require('../../utils/error_message.js')
 
+const registerationUsernameValidator = body('username').notEmpty().withMessage(errorWithMessage(errorCode.FormError))
+    .matches("^[a-zA-Z0-9_]*$").withMessage(errorWithMessage(errorCode.UsernameFormatError))
+    .custom(value => {
+        return User.findOne({ 'username': value }).then(user => {
+            if (user) {
+                console.log(errorWithMessage(errorCode.UsernameExists))
+                return Promise.reject(errorWithMessage(errorCode.UsernameExists));
+            }
+        });
+    })
+const securePasswordValidator = body('password').matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$").withMessage(errorWithMessage(errorCode.PasswordVulnerable))
+const authenticationUsernameValidator = body('username').notEmpty().withMessage(errorWithMessage(errorCode.FormError))
+const notEmptyPasswordValidator = body('password').notEmpty()
+
 router.get('/', authMiddleware, controllers.getProfile)
 router.patch('/', authMiddleware, controllers.editProfile)
-router.post('/register', [body('username').notEmpty().custom(value => {
-    return User.findOne({ 'username': value }).then(user => {
-        if (user) {
-            return Promise.reject('The username already in use.');
-        }
-    });
-}), body('password').notEmpty()], controllers.register)
-router.post('/auth', [body('username').not().isEmpty(), body('password').not().isEmpty()], controllers.auth)
+router.post('/register', [registerationUsernameValidator, securePasswordValidator], controllers.register)
+router.post('/auth', [authenticationUsernameValidator, notEmptyPasswordValidator], controllers.auth)
 
 
 module.exports = router
