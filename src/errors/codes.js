e24fa4354d3655351fd39aae9cb33b613f970ae6
YeonGyu Kim <code.yeon.gyu@gmail.com>
2020-09-09 17:09:45 +0900

    Fix typos when exporting modules
    Replace exports.default to module.exports

--- src/errors/codes.js
+++ src/errors/codes.js
@@ -1,3 +1,7 @@
-exports.default = {
+module.exports = {
     'FormError': 1,
+    'ParameterError': 2,
+    'UsernameExists': 101,
+    'UsernameFormatError': 102,
+    'PasswordVulnerable': 201
 }
--- src/errors/messages.js
+++ src/errors/messages.js
@@ -1,5 +1,9 @@
 const codes = require('./codes.js')
 
-exports.default = {
+module.exports = {
     [codes.FormError]: "Form error.",
+    [codes.ParameterError]: "Parameter error.",
+    [codes.UsernameExists]: "The username already exists.",
+    [codes.UsernameFormatError]: "The username must be alphanumeric.",
+    [codes.PasswordVulnerable]: "The password must contain at least 8 characters, one uppercase letter, one lowercase letter, one number and one special character."
 }
