9264706e55971f24992267bf6165802ebcc37720
YeonGyu Kim <code.yeon.gyu@gmail.com>
2020-09-20 17:16:25 +0900

    Fix typo
    Change wSchema to Schema

--- src/app/order/models.js
+++ src/app/order/models.js
@@ -2,10 +2,13 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const userSchema = require('../user/models.js')
 
-const orderSchema = new wSchema({
+const orderSchema = new Schema({
     //store: storeSchema,
     //menu: menuSchema,
     orderer: { type: userSchema, required: true },
     menu: { type: integer, required: true },
     table_id: { type: integer, required: true }
+    store_id: { type: integer, required: true }
 })
+
+module.exports = mongoose.model('Order', uerSchema)
