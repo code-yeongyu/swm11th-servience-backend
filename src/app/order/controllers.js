aebfdde0223008983092f29d0cb65411437d2e84
YeonGyu Kim <code.yeon.gyu@gmail.com>
2020-10-05 11:36:29 +0900

    Add feature broadcasting to displays when updating orders

--- src/app/order/controllers.js
+++ src/app/order/controllers.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose')
 const { validationResult } = require('express-validator')
 const Order = mongoose.model('Order')
 const io = require('../../utils/websocket.js')
+const wsUtil = require('../../utils/websockets.js')
+const wsMessageType = require('../../utils/websocket_enum.js')
+
 
 exports.getOrders = async (req, res) => {
     const errors = validationResult(req)
@@ -11,9 +14,9 @@ exports.getOrders = async (req, res) => {
     // place below check user's attached store 
     /**/
     try {
-        let pendingOrders = await Order.find({ 'isPending': true })
+        let unfinished_orders = await Order.find({ 'serving_status': { $ne: 2 } })
         return res.json({
-            "orders": pendingOrders
+            "orders": unfinished_orders
         })
     } catch (err) {
         return res.sendStatus(500) // never can be happened unless server error has occured
@@ -29,9 +32,26 @@ exports.addOrder = async (req, res) => {
         let order = new Order(req.body)
         order.orderer = req.username
         await order.save()
+        wsUtil.broadcast(wsUtil.store, wsUtil.createMessage(wsMessageType.Add, wsMessageType.Order, order))
         return res.sendStatus(200)
     } catch (err) {
         return res.sendStatus(500) // never can be happened unless server error has occured
+    }
+}
+
+exports.updateStatus = async (req, res) => {
+    const order_id = req.param("order_id")
+    if (order_id) {
+        const order = await Order.findOne({ '_id': order_id })
+        if (order.servingStatus == 2) {
+            return res.sendStatus(400)
+        }
+        await order.save()
+        wsUtil.broadcast(wsUtil.store, wsUtil.createMessage(wsMessageType.Update, wsMessageType.Order, order))
+        // codes for notifying to display should be placed here.
+        return res.sendStatus(200)
+    } else {
+        return res.sendStatus(400)
     }
 }
 
